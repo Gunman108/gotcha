ecb84e361f6f33469d0e27a05734ffea9fd8030d
Gunman108 <41013296+Gunman108@users.noreply.github.com>
2021-11-04 12:45:45 -0400

    Class number added and milton email now required

--- src/components/loginButton.js
+++ src/components/loginButton.js
@@ -4,15 +4,37 @@ import {firebase, db} from "../config/firebaseConfig"
 
 
 const LoginButton =()=>{
+
+  const getClass = (email)=>{
+    if(email.includes("22")){
+      return 1;
+    }
+    if(email.includes("23")){
+      return 2;
+    }
+    if(email.includes("24")){
+      return 3;
+    }
+    if(email.includes("25")){
+      return 4;
+    }
+  }
     
   const SignInWithFirebase = ()=>{
     var google_provider = new firebase.auth.GoogleAuthProvider()
     firebase.auth().signInWithPopup(google_provider)
     .then((re)=>{
-      return db.collection('users').doc(re.user.uid).set({
-        name: firebase.auth().currentUser.displayName,
-        email: firebase.auth().currentUser.email,
-      })
+      if(firebase.auth().currentUser.email.includes("milton.edu")){
+        return db.collection('users').doc(re.user.uid).set({
+          name: firebase.auth().currentUser.displayName,
+          email: firebase.auth().currentUser.email,
+          class: getClass(firebase.auth().currentUser.email)
+        })
+      }
+      else{
+        alert("Please use a valid milton.edu email")
+      }
+      
    
     })
     .catch((err)=>{
